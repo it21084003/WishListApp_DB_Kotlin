a7d8a608740edebcae1585a061142e4e5d27d3e4
AnttMinPaing <2023_amp_tau@techseed.jp>
2023-12-13 02:11:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mywishlistapp/data/Wish.kt
@@ -0,0 +1,24 @@
+package com.example.mywishlistapp.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "wish-table")
+data class Wish(
+    @PrimaryKey(autoGenerate = true)
+    val id : Long = 0L,
+    @ColumnInfo(name = "wish-title")
+    val title : String = "",
+    @ColumnInfo(name = "wish-desc")
+    val description : String = ""
+)
+
+object DummyWish{
+    val wishList = listOf(
+        Wish(title = "Google Watch 2", description = "An android Watch"),
+        Wish(title = "Oculuc Quest 2", description = "A VR headset for playing games"),
+        Wish(title = "A Sci-fi", description = "Book"),
+        Wish(title = "Bean bag", description = "A comfy bean bag to substutute for a chair")
+    )
+}
